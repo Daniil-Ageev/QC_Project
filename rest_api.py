2495f3ae0aa1aa1548e855c69274b556e72b3c5b
Daniil-Ageev <unselts@gmail.com>
2018-07-20 20:46:52 +0300

    add demo project and change Python version to 2.7.9

--- rest_api.py
+++ rest_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 class QcRestApi:
@@ -15,10 +16,10 @@ class QcRestApi:
             "Content-Type": "application/x-www-form-urlencoded",
             'Host': 'login.software.microfocus.com'
         }
-        self.__domain = "DEFAULT_371612180"
-        self.__project = "371612180_DEMO"
+        self.domain = "DEFAULT_371612180"
+        self.project = "371612180_DEMO"
 
-    def auth(self):
+    def sign_in(self):
         """Sign in function.
 
         Login and password from self.input_auth
@@ -30,8 +31,10 @@ class QcRestApi:
 
         print("Try to Sign in")
         r = self.__session.post(self.url_log, data=self.input_auth, headers=self.__headers)
+        print(self.__session.cookies)
         if 'LWSSO_COOKIE_KEY' in r.cookies.get_dict():
             self.__session.headers.update({'LWSSO_COOKIE_KEY': r.cookies.get('LWSSO_COOKIE_KEY')})
+            print(self.__session.cookies)
             r = self.__session.post(self.url + self.site_session, headers=self.__headers)
             if 'QCSession' in r.cookies.get_dict():
                 self.__session.headers.update({'QCSession': r.cookies.get('QCSession')})
@@ -53,6 +56,7 @@ class QcRestApi:
             Status code from request.
         
         """
+
         print("Try to close a session")
         r = self.__session.delete(self.url + self.site_session, headers=self.__headers)
         if r.status_code == 200:
@@ -68,7 +72,7 @@ class QcRestApi:
             Status code from request.
 
         """
-        
+
         print("Try to extending a session")
         r = self.__session.get(self.url + self.site_session, cookies=self.__cookies, headers=self.__headers)
         if 'LWSSO_COOKIE_KEY' in r.cookies.get_dict():
@@ -78,8 +82,71 @@ class QcRestApi:
         else:
             return r.status_code
 
-    def get_enity(self):
-        r = self.__session.get(self.url + 'rest/domains/{0}/projects/{1}/defects/{2}'.
-                               format(self.__domain, self.__project, "1"),
+    def get_entity(self):
+        """Get entity from defects.
+
+        Returns:
+            Status code from request.
+
+        """
+        r = self.__session.get(self.url + 'rest/domains/{0}/projects/{1}/defects/{2}'
+                               .format(self.domain, self.project, "1"),
                                cookies=self.__cookies, headers=self.__headers)
-        #what to do with xml?
+        print(r.content)
+        return r.status_code
+
+    def create_defect(self):
+        js = """{
+    "data": [
+        {
+            "type": "defect",
+            "name": "Willoughby dissaffected with Marianne",
+            "description": "Refuses to acknowledge her.",
+            "priority": "4-Very High",
+            "detected-by": "steves",
+            "severity": "2-Medium",
+            "creation-time": "20148-07-19"
+        }
+    ]
+}
+        """
+        j = json.dumps([
+        {
+            "type": "defect",
+            "name": "Willoughby dissaffected with Marianne",
+            "description": "Refuses to acknowledge her.",
+            "priority": "4-Very High",
+            "detected-by": "steves",
+            "severity": "2-Medium",
+            "creation-time": "2018-07-19"
+        }
+    ])
+
+        jso = {
+            "Fields": [
+            {
+               "Name": "detected-by",
+               "Value": "henry_tilney"
+            },
+            {
+               "Name": "creation-time",
+               "Value": "2010-03-02"
+            },
+            {
+               "Name": "severity",
+               "Value": "2-Medium"
+            },
+            {
+               "Name": "name",
+               "Value": "Returned value not does not match value in database."
+            }]
+        }
+
+        self.__session.headers.update({'Content-Type': 'application/json'})
+        self.__session.headers.update({'Accept': 'application/json'})
+
+        r = self.__session.post(self.url + 'rest/domains/{0}/projects/{1}/defects'
+                                .format(self.domain, self.project), data=json.dumps(jso))
+        print(r.status_code)
+        #print(r.text)
+
